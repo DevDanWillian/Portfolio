3ba2ca55b7d9ed64a341764605d624a10a3a81ba
DevDanWillian <devdanielwillian@gmail.com>
2023-06-30 21:25:16 -0300

    Title e menu

---
+++ script.js
@@ -0,0 +1,7 @@
+var inicio =document.getElementById("inicio");
+var nav_header=document.getElementsByClassName("nav_header");
+var nav=
+    nav_inicio=document.getElementById("nav_inicio");
+    nav_sobre=document.getElementById("nav_sobre");
+    nav_projetos=document.getElementById("nav_projetos");
+    nav_contato=document.getElementById("nav_contato");
