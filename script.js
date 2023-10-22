9adb16ff7e899553159ffba93b2509bc2e888c57
DevDanWillian <devdanielwillian@gmail.com>
2023-10-22 13:44:26 -0300

    commit

--- script.js
+++ script.js
@@ -8,20 +8,3 @@ var nav= function(){
     nav_contato=document.getElementById("nav_contato");
 }
 var logoface=document.getElementById("logoface");
-
-//quando a tela descer o #inicio deixa de ser opaco lentamente, mas quando voltar ao topo deixa de ser inisivel
-window.onscroll = function(){
-    ini.style.opacity = 1 - window.scrollY / 230;
-}
-
-//quando a largura da pagina for maior ou menor que 290px todo o conteudeo de ini ficara 50% menor
-
-window.onresize = function(){
-    if(window.innerWidth < 290){
-        logoface.style.width = "30%";
-        nav_header.style.width = "97%";
-    }else{
-        ini.style.width = "auto";
-    }
-
-}
